90470a19e47abcbf6cdc39d081129875e0be4c8b
Clément Laforet <sheepkiller@cotds.org>
2018-03-02 22:02:39 +0100

    Add readiness checks [WIP] init-cluster

--- src/main/docker/rabbitmq-wrapper.sh
+++ src/main/docker/rabbitmq-wrapper.sh
@@ -12,15 +12,17 @@ export RABBITMQ_SASL_LOG="-"
 export RABBITMQ_LOGS="-"
 export RABBITMQ_CONFIG_FILE="${MESOS_SANDBOX}/rabbitmq"
 export RABBITMQ_HIGH_MEMORY_WATERMARK=128MiB
+#
+RABBITMQ_CFG_ADVANCED_URL=${RABBITMQ_CFG_ADVANCED_URL:-}
 
 # to shell
 if [ "x${RABBITMQ_CFG_ADVANCED_URL}" != "x" ]
 then
-ADVANCED_FILE=$(basename "${RABBITMQ_CFG_ADVANCED_URL}")
-[ "x${ADVANCED_FILE}" = "x" ] && echo "ADVANCED_FILE is empty" && exit 1
-[ "${ADVANCED_FILE}" != "rabbitmq.advanced" ] && \
-               mv "${MESOS_SANDBOX}/${ADVANCED_FILE}" "${MESOS_SANDBOX}/rabbitmq.advanced.config"
-export ADVANCED_CONFIG_FILE="${MESOS_SANDBOX}/rabbitmq.advanced"
+    ADVANCED_FILE=$(basename "${RABBITMQ_CFG_ADVANCED_URL}")
+    [ "x${ADVANCED_FILE}" = "x" ] && echo "ADVANCED_FILE is empty" && exit 1
+    [ "${ADVANCED_FILE}" != "rabbitmq.advanced" ] && \
+        mv "${MESOS_SANDBOX}/${ADVANCED_FILE}" "${MESOS_SANDBOX}/rabbitmq.advanced.config"
+    export ADVANCED_CONFIG_FILE="${MESOS_SANDBOX}/rabbitmq.advanced"
 fi
 
 export RABBITMQ_MNESIA_BASE="${MESOS_SANDBOX}/rabbitmq-data/mnesia"
@@ -47,6 +49,26 @@ case "$1" in
         touch "${SHAREDDIR}/.init_done"
       }
       ;;
+
+  "health-check")
+      gosu rabbitmq rabbitmqctl  list_vhosts > /dev/null  2>&1
+      ;;
+
+  "health-check-etcd")
+      curl -Ss -L "http://${MESOS_CONTAINER_IP}:${ETCD_PORT}/health" | grep -Eq '^{"health": "true"}$'
+      ;;
+
+  "init-cluster")
+      load_shared_env
+      HOSTNAME="rabbitmq-${POD_INSTANCE_INDEX}-node.${FRAMEWORK_HOST}"
+      RABBITMQ_NODENAME="rabbitmq@${HOSTNAME}"
+      # set cluster name
+      gosu rabbitmq rabbitmqctl -n "${RABBITMQ_NODENAME}" set_cluster_name "${RABBITMQ_CFG_CLUSTER_NAME}"
+      # set password to temp password
+      # XXX less crappy
+      #gosu rabbitmq rabbitmqctl change_password guest "${TMP_GUEST_PASSWORD:-guest}"
+      ;;
+
   *)
       echo "unknown operation"
       exit 1
